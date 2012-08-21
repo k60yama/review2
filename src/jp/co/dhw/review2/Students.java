f08963f4e97c1b78ce9e2e4161d22ff49a17e580
k60yama <k60yama@gmail.com>
2012-08-21 22:07:09 +0900

    second commit

---
+++ src/jp/co/dhw/review2/Students.java
@@ -0,0 +1,74 @@
+package jp.co.dhw.review2;
+
+public class Students extends User{
+
+	public static void main(String[] args){
+
+		//インスタンス化
+		Students takeda_s = new User(1, "武田", "憲隆");
+		takeda_s.showName();
+
+		Students takagi_s = new User(2, "高木", "香");
+		takagi_s.showName();
+
+		Students tokunaga_s = new User(3, "徳永", "裕介");
+		tokunaga_s.showName();
+
+		Students shibata_s = new User(4, "柴田", "伊織");
+		shibata_s.showName();
+
+		Students nagao_s = new User(5, "長尾", "有希子");
+		nagao_s.showName();
+
+		Students sakami_s = new User(6, "酒見", "真悟");
+		sakami_s.showName();
+
+		Students takesue_s = new User(7, "竹末", "憲和");
+		takesue_s.showName();
+
+		Students kunisaki_s = new User(8, "國崎", "宏治");
+		kunisaki_s.showName();
+
+		Students warizaya_s = new User(9, "割鞘", "洋介");
+		warizaya_s.showName();
+
+		Students enokida_s = new User(10, "榎枝", "大悟");
+		enokida_s.showName();
+
+		Students imada_s = new User(11, "今田", "尭");
+		imada_s.showName();
+
+		Students matuguma_s = new User(12, "松隈", "恭子");
+		matuguma_s.showName();
+
+		Students hanada_s = new User(13, "花田", "恒一");
+		hanada_s.showName();
+
+		Students naitou_s = new User(14, "内藤", "初美");
+		naitou_s.showName();
+
+		Students harada_s = new User(15, "原田", "清司");
+		harada_s.showName();
+
+		Students nagano_s = new User(16, "永野", "修平");
+		nagano_s.showName();
+
+		Students nagino_s = new User(17, "奈木野", "純一");
+		nagino_s.showName();
+
+		Students yamanouchi_s = new User(18, "山之内", "洋平");
+		yamanouchi_s.showName();
+
+		Students kanegae_s = new User(19, "鐘ヶ江", "耕治");
+		kanegae_s.showName();
+
+		Students noguti_s = new User(20, "野口", "龍真");
+		noguti_s.showName();
+
+		Students yamano_s = new User(21, "山野", "兼治");
+		yamano_s.showName();
+
+		Students minagawa_s = new User(22, "皆川", "洋介");
+		minagawa_s.showName();
+	}
+}
